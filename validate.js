78b8c7923e05b0a3a1c872bac2e4336fdb4945f1
Dương Việt Anh <81401290+Anh15052001@users.noreply.github.com>
2021-11-15 20:55:51 +0700

    editview

--- validate.js
+++ validate.js
@@ -1,3 +1,15 @@
+var postAPI='https://seedschool.herokuapp.com/v2/api-docs'
+fetch(postAPI)
+    .then(function (response){
+        return response.json();
+    })
+    .then(function (posts) {
+        console.log(posts)
+    }).catch(function (err){
+        console.log('Có lỗi')
+})
+alert('Hi javascrpi')
+
 function validateform() {
     var name = document.myform.username.value;
     var password = document.myform.password.value;
@@ -9,25 +21,12 @@ function validateform() {
         alert("Mật khẩu phải ít nhất 6 kí tự");
         return false;
     }
-}
-function validateform1() {
-    var name = document.myform1.username1.value;
-    var password = document.myform1.password1.value;
-
-    if (name == null || name == "") {
-        alert("Tài khoản không được để trống");
-        return false;
-    } else if (password.length < 6) {
-        alert("Mật khẩu phải ít nhất 6 kí tự");
-        return false;
-    }
-    var email = document.getElementById('email');
-    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-    if (!filter.test(email.value)) {
-        alert('Hay nhap dia chi email hop le.\nExample@gmail.com');
-        email.focus;
-        return false;
-    }
-    alert("Bạn đã đăng kí thành công !")
-    return true;
+    var postAPI='https://jsonplaceholder.typicode.com/posts'
+    fetch(postAPI)
+        .then(function (response){
+          return response.json();
+    })
+        .then(function (posts) {
+             console.log(posts)
+        })
 }

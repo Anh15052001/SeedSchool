4b48844d906a17911fc02686a1809dfa2e625d3a
cnh1en <cnhien12@gmail.com>
2021-11-19 23:45:15 +0700

    test

--- validate.js
+++ validate.js
@@ -1,41 +1,39 @@
+document.getElementById("myform").addEventListener("submit", (e) => {
+  e.preventDefault();
+  var name = document.myform.username.value;
+  var password = document.myform.password.value;
+  if (name == null || name == "") {
+    alert("Tài khoản không được để trống");
+    return false;
+  } else if (password.length < 6) {
+    alert("Mật khẩu phải ít nhất 6 kí tự");
+    return false;
+  }
+  var postAPI = "https://seedschool.herokuapp.com/api/v1/account/login";
 
+  fetch(postAPI, {
+    method: "POST",
+    body: JSON.stringify({
+      username: name,
 
-
-function validateform() {
-    var name = document.myform.username.value;
-    var password = document.myform.password.value;
-
-    if (name == null || name == "") {
-        alert("Tài khoản không được để trống");
-        return false;
-    } else if (password.length < 6) {
-        alert("Mật khẩu phải ít nhất 6 kí tự");
-        return false;
-    }
-    var postAPI='https://seedschool.herokuapp.com/api/v1/account/login'
-    fetch(postAPI,{
-
-        method:"POST",
-
-        body: JSON.stringify({
-
-            username: "string",
-
-            password: "string"
-
-        }),
-
-        headers: {
-
-            "Content-type": "application/json; charset=UTF-8"
-
-        }
-
+      password: password,
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then(function (response) {
+      return response.json();
     })
-        .then(function (response){
-          return response.json();
+    .then(function (posts) {
+      if (posts.error) {
+        alert("Login failure!");
+        console.log(posts);
+      } else {
+        alert("Login successfully!");
+        console.log(posts);
+        localStorage.setItem("token", posts.token);
+      }
     })
-        .then(function (posts) {
-             console.log(posts)
-        })
-}
+    .catch((error) => console.log(error));
+});

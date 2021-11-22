d029aa481e8fb26bef3d6625e976097be96a8bd7
thangndgit <81653169+thangndgit@users.noreply.github.com>
2021-11-23 01:23:49 +0700

    add statistical.html, modify ui of change-info, change-pass, create-event, create-menu

--- admin/assets/js/catchbutton.js
+++ admin/assets/js/catchbutton.js
@@ -1,72 +1,44 @@
-var del, mod;
+// create-menu.html
 
-$('#add-btn').click(function() {
-    var name = $('#add-event-dialog #add-name'),
-        participant = $('#add-event-dialog #add-participant'),
-        condition = $('#add-event-dialog #add-condition'),
-        time = $('#add-event-dialog #add-time'),
-        date = $('#add-event-dialog #add-date'),
-        place = $('#add-event-dialog #add-place');
-
-    if ($('#event-box-list>#no-event').length) {
-        $('#no-event').remove();
+$('#mod-menu-btn').click(function() {
+    if ($('#mod-menu-btn').text() == "Chỉnh sửa thực đơn") {
+        $('.menu-form').prop('disabled', false);
+        $('#mod-menu-btn').text("Lưu thay đổi");
+        $('#mod-menu-btn').removeClass('green-btn');
+        $('#mod-menu-btn').addClass('blue-btn');
+    } else {
+        $('.menu-form').prop('disabled', true);
+        $('#mod-menu-btn').text("Chỉnh sửa thực đơn");
+        $('#mod-menu-btn').removeClass('blue-btn');
+        $('#mod-menu-btn').addClass('green-btn');
     }
-
-    $('.add-event-btn').before(
-        "<div class='widget-box'>" +
-        "<div class='wc-title'>" +
-        "<h4 class='event-name'>" + name.val() + "</h4>" +
-        "</div>" +
-        "<div class='widget-inner'>" +
-        "<div class='noti-box-list'>" +
-        "<ul>" +
-        "<li>" +
-        "<span class='notification-text'>" +
-        "<span>" + "<strong>" + "Đối tượng:" + "</strong>" + "</span>" +
-        "<span class='event-participant'>" + participant.val() + "</span>" +
-        "</span>" +
-        "</li>" +
-        "<li>" +
-        "<span class='notification-text'>" +
-        "<span>" + "<strong>" + "Điều kiện:" + "</strong>" + "</span>" +
-        "<span class='event-condition'>" + condition.val() + "</span>" +
-        "</span>" +
-        "</li>" +
-        "<li>" +
-        "<span class='notification-text'>" +
-        "<span>" + "<strong>" + "Thời gian:" + "</strong>" + "</span>" +
-        "<span class='event-time'>" + time.val() + "</span>" +
-        "</span>" +
-        "</li>" +
-        "<li>" +
-        "<span class='notification-text'>" +
-        "<span>" + "<strong>" + "Ngày:" + "</strong>" + "</span>" +
-        "<span class='event-date'>" + date.val() + "</span>" +
-        "</span>" +
-        "</li>" +
-        "<li>" +
-        "<span class='notification-text'>" +
-        "<span>" + "<strong>" + "Place:" + "</strong>" + "</span>" +
-        "<span class='event-place'>" + place.val() + "</span>" +
-        "</span>" +
-        "</li>" +
-        "<li style='display: flex; justify-content: flex-end'>" +
-        "<button class='modify-event-btn' data-toggle='modal' data-target='#modify-event-dialog' data-backdrop='static'>" + "Chỉnh sửa" + "</button>" +
-        "<button class='delete-event-btn' data-toggle='modal' data-target='#delete-event-dialog' data-backdrop='static'>" + "Xóa hoạt động" + "</button>" +
-        "</li>" +
-        "</ul>" +
-        "</div>" +
-        "</div>" +
-        "</div>"
-    );
-    $('#add-event-dialog form')[0].reset();
 });
 
-$('.delete-event-btn').click(function() {
+// change-info.html
+
+$('#change-info-btn').click(function() {
+    if ($('#change-info-btn').text() == "Chỉnh sửa thông tin") {
+        $('.info-form').prop('disabled', false);
+        $('#change-info-btn').text("Lưu thay đổi");
+        $('#change-info-btn').removeClass('green-btn');
+        $('#change-info-btn').addClass('blue-btn');
+    } else {
+        $('.info-form').prop('disabled', true);
+        $('#change-info-btn').text("Chỉnh sửa thông tin");
+        $('#change-info-btn').removeClass('blue-btn');
+        $('#change-info-btn').addClass('green-btn');
+    }
+});
+
+// create-event.html
+
+var del;
+
+$(document).on('click', '.del-event-btn', function() {
     del = $(this).parents('.widget-box');
 });
 
-$('#delete-btn').click(function() {
+$(document).on('click', '#del-event', function() {
     del.remove();
 
     if (!$('#event-box-list>.widget-box').length) {
@@ -80,4 +52,95 @@ $('#delete-btn').click(function() {
             "</div>"
         );
     }
+
+    $("html, body").stop().animate({ scrollTop: 0 }, 0, 'swing');
+});
+
+$(document).on('click', '.mod-event-btn', function() {
+    let mod_btn = $(this),
+        this_widget = mod_btn.parents('.widget-box');
+
+    if (mod_btn.text() == "Chỉnh sửa") {
+        this_widget.find('.event-form').prop('disabled', false);
+        mod_btn.text("Lưu lại");
+    } else {
+        this_widget.find('.event-form').prop('disabled', true);
+        mod_btn.text("Chỉnh sửa");
+    }
+});
+
+$(document).on('click', '#add-event', function() {
+    let name = $('#add-event-dialog #add-name'),
+        participant = $('#add-event-dialog #add-participant'),
+        condition = $('#add-event-dialog #add-condition'),
+        time = $('#add-event-dialog #add-time'),
+        date = $('#add-event-dialog #add-date'),
+        place = $('#add-event-dialog #add-place');
+
+    if ($('#event-box-list>#no-event').length) {
+        $('#no-event').remove();
+    }
+
+    $('#event-box-list').prepend(
+        "<div class='widget-box'>" +
+        "<div class='wc-title'>" +
+        "<h4>" + name.val() + "</h4>" +
+        "</div>" +
+        "<div class='widget-inner'>" +
+        "<form class='edit-profile m-b30'>" +
+
+        "<div class='form-group row'>" +
+        "<label class='col-md-2 col-form-label'>" + "Tên hoạt động" + "</label>" +
+        "<div class='col-md-10'>" +
+        "<input class='form-control event-form' type='text' value='" + name.val() + "' disabled>" +
+        "</div>" +
+        "</div>" +
+
+        "<div class='form-group row'>" +
+        "<label class='col-md-2 col-form-label'>" + "Đối tượng" + "</label>" +
+        "<div class='col-md-10'>" +
+        "<input class='form-control event-form' type='text' value='" + participant.val() + "' disabled>" +
+        "</div>" +
+        "</div>" +
+
+        "<div class='form-group row'>" +
+        "<label class='col-md-2 col-form-label'>" + "Điều kiện" + "</label>" +
+        "<div class='col-md-10'>" +
+        "<input class='form-control event-form' type='text' value='" + condition.val() + "' disabled>" +
+        "</div>" +
+        "</div>" +
+
+        "<div class='form-group row'>" +
+        "<label class='col-md-2 col-form-label'>" + "Thời gian" + "</label>" +
+        "<div class='col-md-10'>" +
+        "<input class='form-control event-form' type='time' value='" + time.val() + "' disabled>" +
+        "</div>" +
+        "</div>" +
+
+        "<div class='form-group row'>" +
+        "<label class='col-md-2 col-form-label'>" + "Ngày diễn ra" + "</label>" +
+        "<div class='col-md-10'>" +
+        "<input class='form-control event-form' type='date' value='" + date.val() + "' min='2001-02-11' disabled>" +
+        "</div>" +
+        "</div>" +
+
+        "<div class='form-group row'>" +
+        "<label class='col-md-2 col-form-label'>" + "Địa điểm" + "</label>" +
+        "<div class='col-md-10'>" +
+        "<input class='form-control event-form' type='text' value='" + place.val() + "' disabled>" +
+        "</div>" +
+        "</div>" +
+
+        "<div class='row flex' style='justify-content: flex-end;'>" +
+        "<button class='blue-btn mod-event-btn' type='button' style='margin-right: 15px; text-align: center; width: 100px;'>" + "Chỉnh sửa" + "</button>" +
+        "<button class='red-btn del-event-btn' data-toggle='modal' data-target='#del-event-dialog' data-backdrop='static' type='button' style='margin-right: 15px;'>" + "Xóa hoạt động" + "</button>" +
+        "</div>" +
+        "</form>" +
+        "</div>" +
+        "</div>"
+    );
+
+    $('#add-event-dialog form')[0].reset();
+
+    $("html, body").stop().animate({ scrollTop: 0 }, 500, 'swing');
 });

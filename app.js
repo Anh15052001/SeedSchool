c232566fcdf29254710444245b90ee9871fba171
cnh1en <cnhien12@gmail.com>
2021-11-29 22:03:46 +0700

    chinh sua dang nhap

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-const bars = document.querySelector('.bars')
-const list = document.querySelector('.nav-list')
+const bars = document.querySelector(".bars");
+const list = document.querySelector(".nav-list");
 
 bars.addEventListener("click", () => {
-    list.classList.toggle('show')
-})
+  list.classList.toggle("show");
+});
+const token = localStorage.getItem("token");
+function change(e) {
+  const token = localStorage.getItem("token");
+  if (!token) {
+    window.location.replace("./login.html");
+  } else {
+    localStorage.removeItem("token");
+    window.location.replace("./index.html");
+  }
+}
+if (token) {
+  document.getElementById("login").innerHTML = "Đăng xuất";
+}
